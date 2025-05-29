3ff29bad358d70d49286004d373fc5b5ecfd4734
Mark <markadreianramos@gmail.com>
2025-05-29 16:11:52 +0800

    mobile and tablet responsive layout for login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,33 +1,53 @@
 import { type JSX } from "react";
-import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@/components/login-form";
-
+import DesktopImage from "@/assets/LoginPage/login-desktop.png";
+import TabletImage from "@/assets/LoginPage/login-tablet.png";
+import { ScreenSize } from "@/types/utils";
+import Logo from "@/assets/GlobalAssets/Logo.png";
 const Login: () => JSX.Element = () => {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
+    <main className="grid min-h-screen md:grid-cols-1 md:grid-row-2 xl:grid-row-1 xl:grid-cols-2">
+      <div className="flex flex-col md:row-[2] xl:row-[1] xl:col-[2]">
+        <div className="flex justify-center items-center pl-6 pt-6 pr-6 flex-col mb-16 lg:mb-0">
+          <div className="w-xs md:w-[32.75rem]">
+            <a href="#" className="flex items-center gap-2 font-medium mb-6">
+              <img src={Logo} alt="logo" className="w-32 h-32 object-cover" />
+            </a>
+
+            <div className="w-full">
+              <p className="text-base mb-2">ADMIN LOGIN</p>
+              <div>
+                <h1 className="text-3xl font-bold">Cabuyao Central School</h1>
+                <p className="text-base">SPED Learning Intervention Plarform</p>
+              </div>
             </div>
-            Acme Inc.
-          </a>
+          </div>
         </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+        <div className="flex flex-1 items-start justify-center">
+          <div className="w-full p-6 border-border border-2 rounded-2xl w-xs md:w-[32.75rem]">
             <LoginForm />
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+      <div className="relative hidden bg-muted md:block">
+        <picture className="h-screen">
+          {/* Desktop should be first since it is more specific */}
+          <source
+            srcSet={DesktopImage}
+            media={`(min-width: ${ScreenSize.DESKTOP})`}
+          />
+          <source
+            srcSet={TabletImage}
+            media={`(min-width: ${ScreenSize.TABLET})`}
+          />
+          <img
+            src={TabletImage} // fallback if no source matches
+            alt="Cabuyao Central School"
+            className="w-full md:h-40 xl:h-full md:object-cover"
+          />
+        </picture>
       </div>
-    </div>
+    </main>
   );
 };
 
---
+++ client/src/types/utils.ts
@@ -0,0 +1,5 @@
+export enum ScreenSize {
+    MOBILE = "336px",
+    TABLET = "768px",
+    DESKTOP = "1440px",
+}
